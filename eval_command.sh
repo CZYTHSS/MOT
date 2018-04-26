94e94dbb04b7b22ac268cf0a0969c3966657ec0f
CZYTHSS <chenzhengyu14thss@163.com>
2018-04-27 01:45:04 +0800

    upload origin deep_sort result on MOT16_train

--- eval_command.sh
+++ eval_command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-python evaluate_motchallenge.py \
-    --mot_dir=./MOT16/train \
-    --output_dir=./ds_origin_output \
-    --detection_dir=./resource/detections/MOT16_train \
+python3 evaluate_motchallenge.py \
+    --mot_dir=/Users/mark/MOT/MOT16/train \
+    --output_dir=/Users/mark/MOT/ds_origin_output \
+    --detection_dir=/Users/mark/MOT/resources/detections/MOT16_train \
     --min_confidence=0.3 \
     --nn_budget=100 
