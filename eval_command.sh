7cc402fafeee04ad56a6380de31450af4c4243da
CZYTHSS <chenzhengyu14thss@163.com>
2018-04-27 01:54:55 +0800

    upload origin deep_sort result on MOT16_POI_train

--- eval_command.sh
+++ eval_command.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python3 evaluate_motchallenge.py \
     --mot_dir=/Users/mark/MOT/MOT16/train \
-    --output_dir=/Users/mark/MOT/ds_origin_output \
-    --detection_dir=/Users/mark/MOT/resources/detections/MOT16_train \
+    --output_dir=/Users/mark/MOT/ds-origin-output-poi \
+    --detection_dir=/Users/mark/MOT/resources/detections/MOT16_POI_train \
     --min_confidence=0.3 \
     --nn_budget=100 
